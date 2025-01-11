9d330159ecd8f95a8be2612e57365f57231a2fe1
Anshal <104653839+Anshalll@users.noreply.github.com>
2025-01-11 14:10:21 +0530

    Basic

--- server/main.py
+++ server/main.py
@@ -1,10 +1,16 @@
 from flask import Flask, Response, jsonify
+import os
+
 app = Flask(__name__)
 
 
 @app.route('/' , methods=["GET" , "POST"])
 def INDEX():
     return jsonify(message= "project starts")
+
+@app.route("/hello")
+def Sendpath():
+    return jsonify(path=os.getcwd())
 
 
 
