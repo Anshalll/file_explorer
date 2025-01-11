6cfa9261b9dde7cdc3a3e68d2ab76f3e6d978b19
PushkarSingh20 <roshanisingh481@gmail.com>
2025-01-11 14:06:28 +0530

    created flask server

--- server/main.py
+++ server/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from flask import Flask, Response, jsonify
+app = Flask(__name__)
+
+
+@app.route('/' , methods=["GET" , "POST"])
+def INDEX():
+    return jsonify(message= "project starts")
+
+
+
+app.run(debug=True)
